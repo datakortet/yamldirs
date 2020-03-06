f7fc309f5dc959a1c627a96ef698b16705d0e913
Bjorn <bp@datakortet.no>
2020-03-06 14:48:55 +0100

    update list of supported python versions

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@
 """
 import sys
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 
 
 classifiers = """\
@@ -14,32 +13,14 @@ Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
 Topic :: Software Development :: Libraries
 """
 
 version = '1.1.8'
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 
 setup(
@@ -54,7 +35,6 @@ setup(
     description=__doc__.strip(),
     classifiers=[line for line in classifiers.split('\n') if line],
     long_description=open('README.rst').read(),
-    cmdclass={'test': PyTest},
     entry_points={
         'console_scripts': [
             'yamldirs = yamldirs.yamldirs_cmd:main',
