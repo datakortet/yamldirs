dc06b62faf2b90d3be9ad38936f10fb55fef7606
Bjorn Pettersen <bp@norsktest.no>
2021-10-06 19:22:01 +0200

    upversion

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ Programming Language :: Python :: 3.8
 Topic :: Software Development :: Libraries
 """
 
-version = '1.1.12'
+version = '1.1.13'
 
 
 setup(
--- yamldirs/__init__.py
+++ yamldirs/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '1.1.12'
+__version__ = '1.1.13'
 
 from .filemaker import create_files     # noqa
